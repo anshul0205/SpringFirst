4814938634cf611734bd24ab3d1581d0c950ad08
Anshul Singh Sengar <anshulsengar0205@gmail.com>
2025-06-25 15:30:08 +0530

    commented MyController.java

--- src/main/java/com/cdac/Springfirst/controller/MyController.java
+++ src/main/java/com/cdac/Springfirst/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
 	
-	
+	// get home request
 	@RequestMapping("/")
 	public String getHome() {
 		return "home";
